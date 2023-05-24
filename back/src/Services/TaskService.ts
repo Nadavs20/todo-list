653d57ae3d861e970510d7b24dcab8a92caaefc6
Nadav <213531437@idf.il>
2023-05-24 10:59:32 +0300

    fix(TaskService.ts): fix TaskService try-catch blocks duplicity

--- back/src/Services/TaskService.ts
+++ back/src/Services/TaskService.ts
@@ -4,45 +4,27 @@ import { getRepository } from "typeorm";
 export const getAllTasks = async () => getRepository(Task).find();
 
 export const getTaskById = async (id: number) => {
-  try {
-    return getRepository(Task).findOne({ where: { id } });
-  } catch (err) {
-    throw new Error(`Task with id ${id} was not found`);
-  }
+  return getRepository(Task).findOne({ where: { id } });
 };
 
 export const createTask = async (task: Task) => {
-  try {
-    return getRepository(Task).save(task);
-  } catch (err) {
-    throw new Error(`Could not create task`);
-  }
+  return getRepository(Task).save(task);
 };
 
 export const updateTask = async (task: Task) => {
-  try {
-    const taskToUpdate = await getRepository(Task).findOne({
-      where: { id: task.id },
-    });
+  const taskToUpdate = await getRepository(Task).findOne({
+    where: { id: task.id },
+  });
 
-    if (taskToUpdate) {
-      taskToUpdate.description = task.description;
-      taskToUpdate.dueDate = task.dueDate;
-      taskToUpdate.status = task.status;
-
-      return await getRepository(Task).save(taskToUpdate);
-    }
-
-    return null;
-  } catch (err) {
-    throw new Error(`Task with id ${task.id} was not found`);
+  if (taskToUpdate) {
+    return getRepository(Task).save(task);
   }
+
+  return null;
 };
 
 export const deleteTask = async (id: number) => {
-  try {
-    await getRepository(Task).delete(id);
-  } catch (err) {
-    throw new Error(`Task with id ${id} does not exist`);
-  }
+  const result = await getRepository(Task).delete(id);
+
+  return result.affected && result.affected > 0;
 };
