d375400bd9f0e81011b0d9a0e2276a5d23c18010
Nadav <213531437@idf.il>
2023-05-28 14:57:37 +0300

    feat(TaskApi.ts): add generalApi to api file

--- front/src/api/TasksApi.ts
+++ front/src/api/TasksApi.ts
@@ -17,10 +17,20 @@ export const createTask = async (task: Task) => {
 };
 
 export const updateTask = async (task: Task) => {
-  const response = await tasksApi.put<Task>(`/`, task);
+  const response = await tasksApi.put<Task>(`/${task.id}`, task);
   return response.data;
 };
 
 export const deleteTask = async (id: number) => {
   await tasksApi.delete(`/${id}`);
 };
+
+const generalApi = {
+  getAll: getAllTasks,
+  getById: getTaskById,
+  createTask: createTask,
+  updateTask: updateTask,
+  deleteTask: deleteTask,
+};
+
+export default generalApi;
