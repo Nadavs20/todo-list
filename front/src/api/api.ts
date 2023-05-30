30ac587134148f0cb973fbd74f23f314eb1c5615
Nadav <213531437@idf.il>
2023-05-30 15:31:46 +0300

    delete api

--- front/src/api/api.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-
-const API_URL = 'http://localhost:3000';
-
-export const tasksApi: AxiosInstance = axios.create({
-  baseURL: `${API_URL}/tasks`,
-});
