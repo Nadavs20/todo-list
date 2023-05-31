08b526cda00be2dc8bf2ba55eddd9a9c61c6baa3
Nadav <213531437@idf.il>
2023-05-31 15:29:47 +0300

    feat(TaskReducer.ts): remove unneccecery

--- front/src/Reducers/TaskReducer.ts
+++ front/src/Reducers/TaskReducer.ts
@@ -11,19 +11,19 @@ const tasksSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {
-    addTask(state: Task[], action: PayloadAction<Task>) {
+    addTask(state, action: PayloadAction<Task>) {
       state.push(action.payload);
     },
-    removeTask(state: Task[], action: PayloadAction<number>) {
+    removeTask(state, action: PayloadAction<number>) {
       return state.filter((task) => task.id !== action.payload);
     },
-    setTaskList(state: Task[], action: PayloadAction<Task[]>) {
+    setTaskList(state, action: PayloadAction<Task[]>) {
       return (state = action.payload);
     },
-    reverseList(state: Task[], action: PayloadAction<void>) {
+    reverseList(state) {
       return state.slice().reverse();
     },
-    updateTask(state: Task[], action: PayloadAction<Task>) {
+    updateTask(state, action: PayloadAction<Task>) {
       return (state = [...state].map((task) =>
         task.id === action.payload.id ? action.payload : task
       ));
